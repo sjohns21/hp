e4c0a8079d0c05f8dca2d85018d460adbf9a5385
Steve Johnson <steve21johnson@gmail.com>
2019-04-08 22:20:04 -0700

    starting new nav

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 // nav bar
-document.querySelector("#nav-button").addEventListener("click", event => {
-  let navContent = document.querySelector("#nav-content");
-  if (navContent.style.display === "") navContent.style.display = "block";
-  else if (navContent.style.display === "block") navContent.style.display = "";
-  event.stopPropagation();
-});
-document.addEventListener("click", event => {
-  let navContent = document.querySelector("#nav-content");
-  if (navContent.style.display === "block" && !event.path.includes(navContent))
-    navContent.style.display = "";
-});
+// document.querySelector("#nav-button").addEventListener("click", event => {
+//   let navContent = document.querySelector("#nav-content");
+//   if (navContent.style.display === "") navContent.style.display = "block";
+//   else if (navContent.style.display === "block") navContent.style.display = "";
+//   event.stopPropagation();
+// });
+// document.addEventListener("click", event => {
+//   let navContent = document.querySelector("#nav-content");
+//   if (navContent.style.display === "block" && !event.path.includes(navContent))
+//     navContent.style.display = "";
+// });
 
 // card slide toggle
 document.querySelectorAll(".slide .feature-title").forEach(heading => {
